b817e9a3eec22fa82c173a27475a2543e338cad7
Kohei SUGI <kohei.sg@gmail.com>
2020-08-21 17:20:46 +0900

    Add delete task on xhr

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -59,6 +59,7 @@ class TasksController < ApplicationController
     respond_to do |format|
       format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
+      format.js { render :destroy }
     end
   end
 
