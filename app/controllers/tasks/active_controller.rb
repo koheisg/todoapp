9c264ebe8836a655c613b0f0e3f236ab1a2b334a
Kohei SUGI <kohei.sg@gmail.com>
2020-08-22 13:13:22 +0900

    Add Active Task views

--- app/controllers/tasks/active_controller.rb
+++ app/controllers/tasks/active_controller.rb
@@ -1,4 +1,5 @@
 class Tasks::ActiveController < ApplicationController
   def index
+    @tasks = Task.active.order(created_at: :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :tasks do
-    get 'active/index'
+    get 'active/index', as: :active
   end
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
