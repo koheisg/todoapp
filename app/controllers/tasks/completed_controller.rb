31b3d04629a7c9d3491ef7597e2608983f0772cc
Kohei SUGI <kohei.sg@gmail.com>
2020-08-22 13:18:02 +0900

    Add Completed Task views

--- app/controllers/tasks/completed_controller.rb
+++ app/controllers/tasks/completed_controller.rb
@@ -1,4 +1,5 @@
 class Tasks::CompletedController < ApplicationController
   def index
+    @tasks = Task.completed.order(created_at: :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace :tasks do
-    get 'completed/index'
-  end
-  namespace :tasks do
     get 'active/index', as: :active
+    get 'completed/index', as: :completed
   end
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
