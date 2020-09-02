4732ad4ebeb55814045c6d07631ce75fadaa7fef
Kohei SUGI <kohei.sg@gmail.com>
2020-09-03 00:14:51 +0900

    Add Auth0#callback method

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -1,2 +1,15 @@
 class Auth0Controller < ApplicationController
+  def callback
+    # This stores all the user information that came from Auth0
+    # and the IdP
+    session[:userinfo] = request.env['omniauth.auth']
+
+    # Redirect to the URL you want after successful auth
+    redirect_to tasks_path
+  end
+
+  def failure
+    # show a failure page or redirect to an error page
+    @error_msg = request.params['message']
+  end
 end
