8b53c37339274d3dcec58084c5e3c6c0c14b7614
Kohei SUGI <kohei.sg@gmail.com>
2020-09-03 01:52:09 +0900

    Use uid

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,12 +4,12 @@ class TasksController < ApplicationController
 
   # GET /tasks
   def index
-    @tasks = Task.where(user_id: @user[:user_id]).order(created_at: :desc)
+    @tasks = Task.where(user_id: @user[:uid]).order(created_at: :desc)
   end
 
   # POST /tasks.js
   def create
-    @task = Task.new(task_params.merge(user_id: @user[:user_id]))
+    @task = Task.new(task_params.merge(user_id: @user[:uid]))
 
     respond_to do |format|
       if @task.save
@@ -23,7 +23,7 @@ class TasksController < ApplicationController
   # PATCH/PUT /tasks/1.js
   def update
     respond_to do |format|
-      if @task.update(task_params.merge(user_id: @user[:user_id]))
+      if @task.update(task_params.merge(user_id: @user[:uid]))
         format.js { render :update }
       else
         format.js { render :error, status: :unprocessable_entity }
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_task
-      @task = Task.where(user_id: @user[:user_id]).find(params[:id])
+      @task = Task.where(user_id: @user[:uid]).find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
