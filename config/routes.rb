d927551fcf9c3d98bd10b487cad0f070594d775f
Kohei SUGI <kohei.sg@gmail.com>
2020-09-03 00:15:41 +0900

    Add callback routes for auth0

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
       get 'completed', to: 'tasks/completed#index'
     end
   end
+
+  get 'auth/auth0/callback' => 'auth0#callback'
+  get 'auth/failure' => 'auth0#failure'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
