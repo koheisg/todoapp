9679381413456c4a1ba17df17166630e0f689f13
Kohei SUGI <kohei.sg@gmail.com>
2020-09-03 00:49:34 +0900

    Fix callback url

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'auth/auth0/callback' => 'auth0#callback'
+  get 'callback' => 'auth0#callback'
   get 'auth/failure' => 'auth0#failure'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
